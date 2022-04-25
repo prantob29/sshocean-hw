860de6f1cca7760973510c49a7036566f8dce5c3
Alex <af88_88@mail.ru>
2022-04-25 16:15:19 +0300

    add sqlite3

---
+++ js/dropDownMenuOnClick.js
@@ -0,0 +1,32 @@
+// const elements = document.getElementsByClassName('menu-item');
+
+// for (i = 0; i < elements.length; i += 1){
+//     elements[i].addEventListener('mousedown', showMenu);
+//     elements[i].addEventListener('mouseleave', hideMenu);
+// }
+
+// function showMenu() {
+//     if (this.children.length > 1) {
+//         this.children[1].style.height = '50px';
+//         this.children[1].style.opacity = '1';
+//         this.children[1].style.overflow = 'visible';
+//     }
+// }
+
+// function hideMenu() {
+//     if (this.children.length > 1) {
+//         this.children[1].style.height = '0';
+//         this.children[1].style.opacity = '0';
+//         this.children[1].style.overflow = 'hidden';
+//     }
+// }
+
+function func() {
+    this.className = 'open';
+}
+
+let listObj = document.querySelectorAll('.block');
+
+for (let i = 0; i < listObj.length; i += 1){
+    listObj[i].onClick = func;
+}
